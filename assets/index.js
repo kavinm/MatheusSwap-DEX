6dc3e4aa549f37952416bb08dd6f4f4d71e4c0ab
Dan <danporeda@yahoo.com>
2023-06-09 12:05:58 -0400

    collected .png images in assets folder

---
+++ assets/index.js
@@ -0,0 +1,19 @@
+import potato from "./potato.png";
+import matheus from "./Matheus.png";
+import lock from "./lock.png";
+import home from "./home.png";
+import gear from "./gear.png";
+import ethereum from "./Ethereum.png";
+import arrowup from "./arrowUp.png";
+import arrowdown from "./arrowDown.png";
+
+export default {
+    potato,
+    matheus,
+    lock,
+    home,
+    gear,
+    ethereum,
+    arrowup,
+    arrowdown,
+};
